f5cd5d5762fd94152592b182ed7af05bc4a3a25b
Michael <m.molin.1987@gmail.com>
2020-03-19 18:09:28 +0100

    Aggiunta chiamata api serie tv

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
-    var baseUrl = "https://api.themoviedb.org/3";
 
+    var noFilm = false;
+    var noSerie = false;
     var source = $("#film-template").html();
     var filmTemplate = Handlebars.compile(source);
+
+    var sourceSerie = $("#serie-template").html();
+    var serieTemplate = Handlebars.compile(sourceSerie);
 
     var sourceErrore = $("#errore-template").html();
     var erroreTemplate = Handlebars.compile(sourceErrore);
@@ -11,11 +15,9 @@ $(document).ready(function () {
 
     $('#btn-search').click(function() {
         cercaTitolo();
-
     });
 
     $('#input-bar').keypress(function(event) {
-        console.log('tasto premuto');
         if(event.key == "Enter") {
             if($('#input-bar').val().trim().length == 0) {
                 alert('Non hai inserito nessuna parola o lettera!');
@@ -23,10 +25,12 @@ $(document).ready(function () {
                 cercaTitolo();
             }
         }
+
     });
 
 
     function cercaTitolo(){
+        var baseUrl = "https://api.themoviedb.org/3";
         var risultatoInput = $('#input-bar').val();
         $.ajax({
             url: baseUrl + "/search/movie",
@@ -40,7 +44,8 @@ $(document).ready(function () {
                 var films = data.results;
                 if (films.length == 0) {
                     // alert('nessun risultato disponibile');
-                    $('.container').append(erroreTemplate);
+                    noFilm = true;
+                    console.log('no film trovati');
                 } else {
                 for (var i = 0; i < films.length; i++) {
                     // var filmCompleto = films[i];
@@ -53,28 +58,101 @@ $(document).ready(function () {
                     }
                     var filmCompilato = filmTemplate(film);
                     $('.container').append(filmCompilato);
+                    noFilm = false;
                     }
                 }
             },
             error: function () {
-                alert('ERRORISSIMO');
+                alert('ERRORISSIMO FILM');
             }
         })
+        $.ajax({
+            url: baseUrl + "/search/tv",
+            data: {
+                api_key: 'e4a6a0f5b61f8597156e751b684b7437',
+                query: risultatoInput,
+                language: 'it-IT'
+            },
+            success: function (data) {
+                $('.serie').remove();
+                var series = data.results;
+                if (series.length == 0) {
+                    console.log('no serie tv trovate');
+                    noSerie = true;
+                } else {
+                for (var i = 0; i < series.length; i++) {
+                    // var filmCompleto = films[i];
+                    var serie = {
+                        titoloSerie: series[i].name,
+                        titoloOriginaleSerie: series[i].original_name,
+                        linguaSerie: series[i].original_language,
+                        // voto: films[i].vote_average
+                        votoSerie: setStelle(series[i].vote_average)
+                    }
+                    var serieCompilata = serieTemplate(serie);
+                    $('.container').append(serieCompilata);
+                    noSerie = false;
+                    }
+                }
+            },
+            error: function () {
+                alert('ERRORISSIMO SERIE TV');
+            }
+        })
+        console.log('la variabile No Film è ', noFilm);
+        console.log('la variabile No serie tv è ', noSerie);
+        if ((noSerie == true) && (noFilm == true)) {
+            $('.container').append(erroreTemplate);
+        }
         $('#input-bar').val('');
     }
+
+    // function cercaSerie(){
+    //     var baseUrl = "https://api.themoviedb.org/3";
+    //     var risultatoInput = $('#input-bar').val();
+    //     $.ajax({
+    //         url: baseUrl + "/search/tv",
+    //         data: {
+    //             api_key: 'e4a6a0f5b61f8597156e751b684b7437',
+    //             query: risultatoInput,
+    //             language: 'it-IT'
+    //         },
+    //         success: function (data) {
+    //             $('.serie').remove();
+    //             var series = data.results;
+    //             if (series.length == 0) {
+    //                 console.log('no serie tv trovate');
+    //                 noSerie = true;
+    //             } else {
+    //             for (var i = 0; i < series.length; i++) {
+    //                 // var filmCompleto = films[i];
+    //                 var serie = {
+    //                     titoloSerie: series[i].name,
+    //                     titoloOriginaleSerie: series[i].original_name,
+    //                     linguaSerie: series[i].original_language,
+    //                     // voto: films[i].vote_average
+    //                     votoSerie: setStelle(series[i].vote_average)
+    //                 }
+    //                 var serieCompilata = serieTemplate(serie);
+    //                 $('.container').append(serieCompilata);
+    //                 noSerie = false;
+    //                 }
+    //             }
+    //         },
+    //         error: function () {
+    //             alert('ERRORISSIMO SERIE TV');
+    //         }
+    //     })
+    // }
 
     function setStelle (filmVoto) {
         var voto = Math.ceil(filmVoto/2);
         var stelle = "";
 
-        console.log('il voto è', voto);
         for (var i = 1; i <= 5; i++) {
-            console.log(i);
             if (i <= voto) {
-                console.log('metto stella piena');
                 stelle += '<i id="stella-piena" class="fas fa-star"></i>';
             } else {
-                console.log('metto stella vuota');
                 stelle += '<i id="stella-piena" class="far fa-star"></i>';
             }
         }
